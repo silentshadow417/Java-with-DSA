57877374866fbd339778d61c97bf72c756601074
Silent Shadow <silentshadow417@gmail.com>
2026-09-01 19:51:28 +0530

    Add 3 new update

---
+++ FactorialFunction.java
@@ -0,0 +1,10 @@
+public static int  Fact(int n) {
+    int f = 1;
+    for(int i=1; i<=n; i++ ){
+        f = f * i;
+    }
+    return f;
+}
+public static void main(String args[]) {
+        System.out.println(Fact(5));
+    }
--- tringlePattern.java
+++ tringlePattern.java
@@ -1,35 +1,4 @@
 public class tringlePattern {
     public static void main(String args[]){
-        int n = 3;
-       int width = 8;
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-
-            for (int j = 1 ; j <= (width - 2 * i); j++ ){
-                System.out.print(" ");
-            }
-
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-
-            
-
-            
-            System.out.println();
-        }
-
-       
-
-    
-
-        //  for(int i = 1; i <= n; i++) {
-        //     for(int j = i; j <= n ; j++) {
-        //             System.out.print("*");
-        //         }
-        // System.out.println();
-        // }
     }
 }
