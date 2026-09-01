7b47629572d6c46ec6fd201368af29004e24b382
Silent Shadow <silentshadow417@gmail.com>
2026-09-01 20:12:19 +0530

    prime submit

---
+++ prime.java
@@ -0,0 +1,16 @@
+public class prime {
+public static boolean isPrime(int n) {
+    boolean isPrime = true;
+    for(int i = 2; i<n ; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+}
+
+    public static void main(String args[]) {
+        System.out.println(isPrime(12));
+    }
+}
