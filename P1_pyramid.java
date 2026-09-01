8f7953e6f563c96232606b59e9640441b0bb3b4d
Silent Shadow <silentshadow417@gmail.com>
2026-09-01 19:28:49 +0530

    submit

--- P1_pyramid.java
+++ P1_pyramid.java
@@ -3,7 +3,7 @@ public class P1_pyramid {
         int n = 5;
         
         for(int i = 1; i <= n; i++) {
-            for(int j = i; j <= n ; j++) {
+            for(int j = 1; j <= i ; j++) {
                     System.out.print("*");
                 }
         System.out.println();
