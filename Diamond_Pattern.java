c748525079703ccf8445c4d4fbf4e117213762f9
Silent Shadow <https://github.com/silentshadow417.com>
2026-09-03 20:28:08 +0530

    changes submit

--- Diamond_Pattern.java
+++ Diamond_Pattern.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Diamond_Pattern {
     public static void diamond(int n) {
         for(int i = 1; i<=n; i++){
@@ -28,35 +27,4 @@ public class Diamond_Pattern {
         diamond(5);
     }
 }
-=======
-public class Diamond_Pattern {
-    public static void diamond(int n) {
-        for(int i = 1; i<=n; i++){
-            for(int j =1; j<=(n-i); j++) {
-                System.out.print(" ");
-            }
-
-            for(int j = 1; j<=(2*i) -1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-         for(int i=n ; i>=1; i--){
-            for(int j =1; j<=(n-i); j++) {
-                System.out.print(" ");
-            }
-
-            for(int j = 1; j<=(2*i) -1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String args[]) {
-        diamond(5);
-    }
-}
->>>>>>> c8894456cbc201b9992aa458438f7072f2f66ddc
 // (2*i)-1
