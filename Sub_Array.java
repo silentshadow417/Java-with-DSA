eaba15e46c15406418970eb24b98b19757f7d670
Silent Shadow <https://github.com/silentshadow417.com>
2026-09-04 20:53:09 +0530

    change

--- Sub_Array.java
+++ Sub_Array.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Sub_Array {
     public static void Sub(int num[]) {
+        int ts = 0;
         for(int i=0; i<num.length; i++) {
             int start = i;
             for(int j =i; j<num.length; j++) {
@@ -8,10 +9,12 @@ public class Sub_Array {
                 for(int k = start; k<= end; k++){
                     System.out.print(num[k]+" ");
                 }
+                ts++;
                 System.out.println();
             }
              System.out.println();
         }
+        System.out.println("Total Sub Array: " + ts);
     }
 
     public static void main(String args[]) {
